83b05fed351d1c8a8b16a765c754298dabeb4579
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2021-10-28 15:34:03 +0900

    playSoundの追加
    取得した音がなっている模様。まだバグあり

---
+++ Assets/Script/Mic.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.UI;
+
+
+public class Mic : MonoBehaviour
+{
+    private NoteNameDetector notename;
+    private Text text;
+    AudioSource aud; //マイクの音を読み取ったり内部再生する用
+    public AudioSource aud2; //初音ミクの声を再生する
+    public AudioClip[] clips;
+
+    void Start()
+    {
+        aud = GetComponent<AudioSource>();
+        text = GetComponentInChildren<Text>();
+        //Debug.Log(text);
+        notename = new NoteNameDetector();
+        // マイク名、ループするかどうか、AudioClipの秒数、サンプリングレート を指定する
+        aud.clip = Microphone.Start(null, true, 1, 44100);
+        aud.Play();
+        //aud2.clip = clips[0];
+        //aud2.Play();
+        //aud.PlayOneShot(sound);
+
+        text.text = "読み取り中";
+    }
+    void Update()
+    {
+
+
+        if (Time.frameCount % 50 != 0) { return; }
+        Debug.Log("UPDATE!");
+        float[] spectrum = new float[1024];
+        aud.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
+        var maxIndex = 0;
+        var maxValue = 0.0f;
+        var freq = 0.0f;
+
+        for (int i = 0; i < spectrum.Length; i++)
+        {
+            var val = spectrum[i];
+            if (val > maxValue)
+            {
+                maxValue = val;
+                maxIndex = i;
+                // maxValue が最も大きい周波数成分の値で、
+                // maxIndex がそのインデックス。欲しいのはこっち。
+            }
+
+            //Debug.Log("max: " + maxIndex);
+            //Debug.Log("Audio:" + AudioSettings.outputSampleRate);
+            //Debug.Log("spectrum: " + spectrum.Length);
+            freq = maxIndex * AudioSettings.outputSampleRate / 2 / spectrum.Length;
+            Debug.Log("freq:" + freq);
+            try
+            {
+                Debug.Log("notename" + notename.GetNoteName(freq));
+                text.text = notename.GetNoteName(freq);
+                notename.soundPlay(freq, this);
+                //freqに0が入ると例外が起こる(なぜ0が多いのか)
+
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
+
+    }
+
+}
+
+public class NoteNameDetector
+{
+    Mic mic;
+    private string[] noteNames = { "ド", "ド♯", "レ", "レ♯", "ミ", "ファ", "ファ♯", "ソ", "ソ♯", "ラ", "ラ♯", "シ" };
+
+    public string GetNoteName(float freq)
+    {
+        //Debug.Log("GetNoteName: "+freq);
+        // 周波数からMIDIノートナンバーを計算
+        var noteNumber = calculateNoteNumberFromFrequency(freq); //0から100くらい？Midiに対応している 全てのオクターブを重ねるのもあり
+                                                                 // 0:C - 11:B に収める
+        var note = noteNumber % 12; //あまりではなくて商がオクターブに対応しているかも
+        Debug.Log("note:" + note);
+        // 0:C～11:Bに該当する音名を返す
+        return noteNames[note];
+    }
+    public void soundPlay(float freq, Mic mic)
+    {
+        Debug.Log("soundPlay: " + freq);
+        var noteNumber = calculateNoteNumberFromFrequency(freq);
+        var note = noteNumber % 12; //0〜11の数字が入る
+        mic.aud2.clip = mic.clips[note];
+        mic.aud2.PlayOneShot(mic.aud2.clip);
+        Debug.Log("soundPlay: " + noteNames[note]);
+
+    }
+
+    // See https://en.wikipedia.org/wiki/MIDI_tuning_standard
+    private int calculateNoteNumberFromFrequency(float freq)
+    {
+        return Mathf.RoundToInt(69 + 12 * Mathf.Log(freq / 440, 2));
+    }
+}
